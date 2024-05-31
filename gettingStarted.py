04c8d7bbe2dcb03aedbb54c8e13dde8b641983aa
hg2896 <160685152+hg2896@users.noreply.github.com>
2024-05-30 19:12:48 -0700

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -18,7 +18,7 @@ def welcome_assignment_answers(question):
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
         answer = "4deefd4471b0cf01afeea837fd61f9edc4a36f8c73959bd030517586151c6a98"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = 5
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
